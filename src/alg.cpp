871c28c3886d6ca6898762f78c3b3eeab2598c05
pomelovaksenia <116708935+pomelovaksenia@users.noreply.github.com>
2023-06-04 23:08:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,35 @@
 #include  <cstdlib>
 #include  "bst.h"
 
+bool isLetter(char ch) {
+    return ch >= 'a' && ch <= 'z';
+}
+
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST <std::string> tree;
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return tree;
+  } else {
+     while (!file.eof()) {
+     std::string temp = "";
+     while (true) {
+       char c;
+       c = file.get();
+       if (isLetter(c)) {
+         temp += c;
+       } else if (c >= 'A' && c <= 'Z') {
+           c = tolower(c);
+           temp += c;
+           } else {
+                break;
+            }
+     }
+       tree.add(temp);
+       temp.clear();
+    }
+        file.close();
+        return tree;
+    }
 }
